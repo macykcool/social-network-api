6f3a931afbd3ad03e8da5d149a1f37116f581769
Macy Kay Mannix <macymannix@gmail.com>
2022-09-07 20:11:04 -0700

    user rte

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,31 @@
 const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/user-controller');
 
+// set up GET all and POST at /api/users
+router
+.route('/')
+.get(getAllUsers)
+.post(createUser);
+
+// Set up GET one, PUT, and DELETE at /api/users/:id
+router
+.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
+
+// Set up PUT new friend and DELETE friend /api/users/:userId/friends/:friendId
+router
+.route('/:id/friends/:friendId')
+.put(addFriend)
+.delete(deleteFriend);
 
 module.exports = router;
