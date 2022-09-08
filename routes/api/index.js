407b91560f9b08e67f2adce2ed8de22a5880beb7
Macy Kay Mannix <macymannix@gmail.com>
2022-09-07 20:21:52 -0700

    thought rte

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-// const { model } = require('mongoose');
-// const thoughtsRoutes = require('./thoughts-routes');
+
+const thoughtsRoutes = require('./thoughts-routes');
 const userRoutes = require('./user-routes');
 
 router.use('/users', userRoutes);
-// router.use('/thoughts', thoughtsRoutes);
+router.use('/thoughts', thoughtsRoutes);
 
 module.exports = router;
---
+++ routes/api/thoughts-routes.js
@@ -0,0 +1,29 @@
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+}= require('../../controllers/thought-controller');
+
+router.route('/').get(getAllThoughts);
+
+router
+.route('/:id')
+.get(getThoughtById)
+.delete(deleteThought)
+.post(updateThought);
+
+router
+.route('/:userId')
+.post(createThought);
+
+router
+.route('/:thoughtsId/reactionsd')
+.post(addReaction);
+
+router
+.route('/:thoughtsId/reactions/:reactionId')
+.delete(deleteReaction);
