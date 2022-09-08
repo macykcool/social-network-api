173a4e22d09914dfd942b0519f74e13a12c84a01
Macy Kay Mannix <macymannix@gmail.com>
2022-09-07 23:03:22 -0700

    debugging

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(require("./routes"));
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/Social-Network-API",
+  process.env.MONGODB_URI || "mongodb://127.0.01/social-network-api",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+
+
 
   mongoose.set('debug', true);
 
